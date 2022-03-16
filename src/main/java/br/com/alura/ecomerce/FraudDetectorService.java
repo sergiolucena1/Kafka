1b67b967b85422202a0c96782861568dec89d6c4
Sergio Carvalho <sergio.carvalho@dock.tech>
2022-03-16 13:01:05 -0300

    parallelism and keys

--- src/main/java/br/com/alura/ecomerce/FraudDetectorService.java
+++ src/main/java/br/com/alura/ecomerce/FraudDetectorService.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.UUID;
 
 public class FraudDetectorService {
     public static void main(String[] args) {
@@ -41,6 +42,7 @@ public class FraudDetectorService {
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());        // transformar a chave de byte pra string
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());              //grupo que recebe todas as mensagens
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "-" + UUID.randomUUID().toString());              //grupo que recebe todas as mensagens
         return properties;
     }
 
--- src/main/java/br/com/alura/ecomerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecomerce/NewOrderMain.java
@@ -4,27 +4,33 @@ import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());                   // Kafkaproducer recebe propriedades(produtor)
-        var value = "13212232,8263263,1234";                                    //  a mensagem(valor) que eu vou mandar
-        //primeiro tópico
-        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);    // Mensagem criada -topico, chave e o valor
-        Callback callback = (data, ex) -> {                                           //enviador de novo pedido
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
-            }
-            System.out.println(("sucesso enviado " + data.topic() + ":::partition" + data.partition() + "/ offset" + data.offset() + "/" + data.timestamp()));
-        };
-        var email = "Thank you for your order! We are processing your order!";
-        //Segundo tópico
-        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
-        producer.send(record, callback).get();                                                                                  //enviar alguma coisa(mensagem, registro ou record) fica registrada no kafka pelo tempo que esta setado no server properties
-        producer.send(emailRecord, callback).get();
+
+        for (var i = 0; i < 100; i++) {                                                 //laço pra enviar mensagem 100 vezes
+
+            var key = UUID.randomUUID().toString();                                 //nossa chave vai ser um id diferente de usuário
+            var value = key + "8263263,1234";                                                //  a mensagem chave + (valor) que eu vou mandar
+            //primeiro tópico
+            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);    // Mensagem criada -topico, chave e o valor
+            Callback callback = (data, ex) -> {                                           //enviador de novo pedido
+                if (ex != null) {
+                    ex.printStackTrace();
+                    return;
+                }
+                System.out.println(("sucesso enviado " + data.topic() + ":::partition" + data.partition() + "/ offset" + data.offset() + "/" + data.timestamp()));
+            };
+            var email = "Thank you for your order! We are processing your order!";
+            //Segundo tópico
+            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
+            producer.send(record, callback).get();                                                                                  //enviar alguma coisa(mensagem, registro ou record) fica registrada no kafka pelo tempo que esta setado no server properties
+            producer.send(emailRecord, callback).get();
+        }
     }
 
     private static Properties properties() { // metodo estático que devolve um properties
